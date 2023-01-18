1f63e3bfd266415a48ca801662807f608354d2dd
Saurab Salhotra <saurabsalhotra@gmail.com>
2023-01-18 13:52:21 +0530

    added table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls:['app.component.css'],
 })
-export class AppComponent {
-  title = 'makerobos';
+export class AppComponent implements OnInit {
+  data: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get('https://jsonplaceholder.typicode.com/posts').subscribe(data => {
+      this.data = data;
+    });
+  }
 }
