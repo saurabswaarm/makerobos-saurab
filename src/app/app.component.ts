53c72f392d4a6b17e30864c9ca606a952733ee82
Saurab Salhotra <saurabsalhotra@gmail.com>
2023-01-18 14:07:03 +0530

    basic implementation complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls:['app.component.css'],
+  styleUrls: ['app.component.css'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   data: any;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.http.get('https://jsonplaceholder.typicode.com/posts').subscribe(data => {
+    this.data ?? this.http.get<any[]>('https://jsonplaceholder.typicode.com/posts').subscribe(data => {
       this.data = data;
+    });
+  }
+
+  makePostRequest() {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    const body = { title: 'Time', body: '3-1-2020' };
+    this.http.post('https://jsonplaceholder.typicode.com/posts', body, { headers }).subscribe(response => {
+      console.log(response);
+      this.data.push(response);
     });
   }
 }
